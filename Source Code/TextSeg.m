4b107992d580c73ef77bfa5e183ed970050d3ee9
Dharik-Anwar <dharikanwar4@gmail.com>
2022-09-08 16:29:04 +0530

    Saved changes in TextSeg.m

--- Source Code/TextSeg.m
+++ Source Code/TextSeg.m
@@ -3,25 +3,35 @@ for i = 1:6
     img = gs;   %Loading the grayscale image
     num = 3;    %Setting the nhood for filter
     
-    filt = myfilter("rang", img, num);  %Calling the range filter
+    filt = myfilter("std", img, num);  %Calling the range filter
     filtBW = ~imbinarize(filt); %Binarizing the filter
     imgBW = img < 250 & img > 80;
     mask = filtBW - imgBW;  %Making the mask
+    maski = imgBW;
+    maskf = filtBW;
 %     
     SE = strel("disk", 4);  %Strutural Element
     mask = imopen(~mask,SE);    %Opening the mask
+    maski = imopen(maski,SE); 
+    maskf = imopen(maskf,SE); 
 %     mask = ~mask;
 %     
-    mask = bwareaopen(mask,10000);  %Crearing the noise in the background
+    mask = bwareaopen(mask,1000);  %Crearing the noise in the background
+    maski = bwareaopen(maski,1000);
+    maskf = bwareaopen(maskf,1000);
 %     mask = ~mask;
     mask = imfill(mask,"holes");    %Filling the holes
+    maski = imfill(maski,"holes");
+    maskf = imfill(maskf,"holes");
 %     mask = imclearborder(mask); %Clearing the border
     
-    img = cat(3,r,g,b); %Making an RGB image
-    img1 = imoverlay(img,~mask,"k");    %Overlay the mask in the image
+    RGB = cat(3,r,g,b); %Making an RGB image
+    maskOverlay = imoverlay(RGB,~mask,"k");    %Overlay the mask in the image
+    maskiOverlay = imoverlay(RGB,~maski,"k");
+    maskfOverlay = imoverlay(RGB,~maskf,"k");
     figure
-    imshowpair(img1,img,"montage")
-%     imshow(mask)
+%     imshowpair(maskOverlay,RGB,"montage")
+    montage({mask,maski,maskf,maskOverlay,maskiOverlay,maskfOverlay},"Size",[2,3])
 end
 
 function res = myfilter(filt, img, num)
@@ -58,3 +68,5 @@ function [r,g,b,gs,l,a1,b1,h,s,v] = Init(num, bool)
         montage({r,g,b,l,a1,b1,h,s,v},"Size",[3,3])
     end
 end
+
+
